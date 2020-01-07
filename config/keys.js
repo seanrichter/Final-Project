d947d63b329138f3c4ac5379ee4b5a9e655bb9a6
Sean Richter <sean.richter04@outlook.com>
2020-01-06 22:29:38 -0600

    added users.js and items.js to backend code for user authentication

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://smrichter:Final-Project@final-project-5ag0e.gcp.mongodb.net/test?retryWrites=true&w=majority"
+};
--- server.js
+++ server.js
@@ -9,20 +9,26 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
+app.use("/api/items", require("./routes/api/items"));
 
 // SAVE THIS STEP UNTIL ROUTES
 app.use(routes);
+app.use("/api/users", require("./routes/api/users"));
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Final-Project",{useNewUrlParser:true,useUnifiedTopology: true});
-
-
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/Final-Project",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  }
+);
 
 // Start the API server
 app.listen(PORT, function() {
